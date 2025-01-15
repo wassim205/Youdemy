7ba5181d3ce43a0bba305a49f589c1cdd2e3edba
Wassim El Mourabit <wassimelmorabit7@gmail.com>
2025-01-15 10:24:53 +0100

    YD-3 YD-2 Modifier les diagramms et son corriger

--- app/config/db.php
+++ app/config/db.php
@@ -5,7 +5,7 @@ class Db {
     public function __construct()
     {
         try {
-            $this->conn = new PDO("mysql:host=localhost;dbname=GestionComptesBancaires", "root", "");
+            $this->conn = new PDO("mysql:host=localhost;dbname=youdemy", "root", "");
             // set the PDO error mode to exception
             $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
           //   echo "Connected successfully";
